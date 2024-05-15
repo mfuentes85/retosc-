e34379ee92db247c2a5e6e40f806dbf5b0df47d6
mfuentes85 <mfuentes85@gmail.com>
2024-05-14 19:20:10 -0600

    Programa Calculadora de Perimetro y Area completado

---
+++ CalcPerimetroArea/Program.cs
@@ -0,0 +1,16 @@
+﻿int perimetro;
+int area;
+
+Console.WriteLine ("Escriba el valor de la base del rectángulo");
+    int brectang= int.Parse(Console.ReadLine ());
+
+Console.WriteLine ("Escriba el valor de la altura del rectángulo");
+    int hrectang= int.Parse(Console.ReadLine ());
+
+perimetro= (2*brectang)+(2*hrectang);
+
+Console.WriteLine ("El perímetro del rectangulo con las medidas proporcionadas es: " +perimetro +" cm");
+
+area= brectang*hrectang;
+Console.WriteLine ("El área del rectangulo con las medidas proporcionadas es: " +area + " cm2");
+
